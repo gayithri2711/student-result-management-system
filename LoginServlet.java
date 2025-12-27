b7ce99a59a00b0c5a1ec7d86215128bab9573ef7
Purnashree <purnashreek5@gmail.com>
2025-12-27 23:50:33 +0530

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet{
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String user = request.getParameter("userid");
+        String pass = request.getParameter("password");
+
+        if (user.equals("purna27") && pass.equals("passme!23")) {
+            response.sendRedirect("home.jsp");
+        } else {
+            response.getWriter().println("Invalid login credentials");
+        }
+    }
+}
